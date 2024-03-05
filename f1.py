49925192b8cc9d9c7bb6f5340eb1bef2acafa111
creatorofSatan <162295257+creatorofSatan@users.noreply.github.com>
2024-03-05 19:52:03 +0330

    Add files via upload

---
+++ f1.py
@@ -0,0 +1,25 @@
+print("""_____xxxxxxxx________xxxxxxxx
+____xxxxxxxxxx______xxxxxxxxxxx
+___xxxxxxxxxxxxx___xxxxxxxxxxxxx
+___xxxxxxxxxxxxxx_xxxxxxxxxxxxxx
+___xxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+____xxxxxxxxxxxxxxxxxxxxxxxxxxxx
+_____xxxxxxxxxxxxxxxxxxxxxxxxxx
+______xxxxxxxxxxxxxxxxxxxxxxx
+_________xxxxxxxxxxxxxxxxxx
+___________xxxxxxxxxxxxx
+_____________xxxxxxxxx
+______________xxxxxx
+_______________xxxx
+_______________xxx
+""")
+print ('████████████████████████████████████████████████████████')
+print ('My child I will be like you a hundred times a day Now if you are alive because of your mother dont be a bully Script made by the creator of Satan')
+('Next time I will ask you to kill your father')
+print ('████████████████████████████████████████████████████████')
+print ('This heart is dedicated to your mother')
+print ('████████████████████████████████████████████████████████')
+print ('creator of Satan')
+print ('████████████████████████████████████████████████████████')
+print ('https://t.me/+TRqsV0sVbM4wNDY0')
+print ('████████████████████████████████████████████████████████')
